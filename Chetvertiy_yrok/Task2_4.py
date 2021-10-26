02c0126984fc1d682f864ace78603b8e35d85aa6
LegendarbIch <90645481+LegendarbIch@users.noreply.github.com>
2021-10-26 12:48:23 +0300

    Add files via upload

---
+++ Chetvertiy_yrok/Task2_4.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+# Задача 2
+
+stroka = input("Введите строку: ")
+a = stroka.count(" ")
+print("Слов в строке: ", a+1)
---
+++ Chetvertiy_yrok/Task4_4.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+# Задача 4
+
+stroka = input("Строка из двух слов: ")
+s1 = stroka.split(" ") 
+print(s1[1] + " " + s1[0])

02dbced7a22bddf59107f7c2bb58f2d67bd4f657
Kirill Chichin <90645481+LegendarbIch@users.noreply.github.com>
2022-05-30 20:49:16 +0300

    Delete Perviy_Yrok directory

--- Perviy_Yrok/Task4.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-
-#Задача 4
-
-seconds = int(input("Введите кол-во секунд "))
-dni = seconds//(3600*24)
-chasi = seconds//3600
-min = seconds//60
-print(dni, ":" , chasi, ":", min, ":", seconds ) 
--- Perviy_Yrok/Task6.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-
-#Задача 6
-
-x = int(input("Введите икс "))
-y = int(input("Введите игрик "))
-x, y = y, x
-print(x,y)

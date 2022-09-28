f69ed2f0f872bbc9b3c35eb1c91230b1e610ec3f
LegendarbIch <cirpich123@mail.ru>
2022-09-28 03:56:15 +0300

    new

--- Java Project STOREHOUSE/Main.java
+++ Java Project STOREHOUSE/Main.java
@@ -2,15 +2,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static void  AddingProduct() {
+        Sklad sklad = new Sklad();
+        sklad.AddNewProduct();
+        sklad.toString();
+    }
     public static void Menu() {
         System.out.println("Выберите, что хотите сделать:  1. Добавить товар на склад" + "\n" +
                            "                               2. Отпустить товар покупателю" + "\n");
         Scanner console = new Scanner(System.in);
         int number = console.nextInt();
         if (number == 1) {
-            Sklad sklad = new Sklad();
-            sklad.AddNewProduct();
-            sklad.toString();
+            AddingProduct();
+            int number2 = 0;
+            while (number2 != 2) {
+                System.out.println("Добавить еще товар?   1." + "\n" +
+                        "Выйти в главное меню? 2." + "\n");
+                number2 = console.nextInt();
+                if (number2 == 1) {
+                    AddingProduct();
+                } else if (number2 == 2) {
+                    System.out.print("\n\n\n\n\n");
+                    Menu();
+                }
+            }
         } else if (number == 2) {
 
         }

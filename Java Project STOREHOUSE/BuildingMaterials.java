783974fd871511c90c4278614eb61a0ebb951b2f
Kirill Chichin <cirpich123@mail.ru>
2022-10-06 21:02:03 +0300

    new

--- Java Project STOREHOUSE/BuildingMaterials.java
+++
@@ -1,6 +0,0 @@
-public class BuildingMaterials extends Product{
-
-    public BuildingMaterials(String ProviderName,String ProductName, int ProductArticle, int Price) {
-        super(ProviderName, ProductName, ProductArticle, Price);
-    }
-}
--- Java Project STOREHOUSE/Technic.java
+++
@@ -1,5 +0,0 @@
-public class Technic extends Product{
-    public Technic(String ProviderName,String ProductName, int ProductArticle, int Price) {
-        super(ProviderName,ProductName, ProductArticle, Price);
-    }
-}
